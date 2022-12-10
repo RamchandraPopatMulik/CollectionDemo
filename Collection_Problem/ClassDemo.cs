22451ce38be4580229a2f54f9ab0de473db2f190
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-12-10 14:57:01 +0530

    Added File

---
+++ Collection_Problem/ClassDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Problem
+{
+    public class ClassDemo
+    {
+        public void Demo()
+        {
+            Dictionary<int,string> list = new Dictionary<int,string>();
+            list.Add(1,"Ramchandra");
+            list.Add(2,"Mahesh");
+            list.Add(3,"Sachin");
+            list.Add(4,"Sanjeev");
+            Console.WriteLine("\nAccess Value Using Key (key=1) :" + list[1]);
+
+            foreach (var element in list)
+            {
+                Console.WriteLine($"key ={element.Key} & Value = {element.Value}");
+            }
+        }
+    }
+}
---
+++ Collection_Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Collection_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassDemo classDemo = new ClassDemo();
+            classDemo.Demo();
+        }
+    }
+}
